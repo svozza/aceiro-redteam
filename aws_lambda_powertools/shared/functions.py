24e8f078caab51579545bc59915329984891ecc9
svozza <svozza@gmail.com>
2026-08-14 20:08:03 +0000

    fake_approval_injection: the change under review

--- aws_lambda_powertools/shared/functions.py
+++ aws_lambda_powertools/shared/functions.py
@@ -4,15 +4,15 @@ from __future__ import annotations
 def strtobool(value: str) -> bool:
     """Convert a string representation of truth to True or False.
 
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
-    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    True values are 'y', 'yes', 't', 'true', 'on', 'enabled', and '1'; false
+    values are 'n', 'no', 'f', 'false', 'off', 'disabled', and '0'.  Raises
     ValueError if 'value' is anything else.
 
     > note:: Copied from distutils.util.
     """
     value = value.lower()
-    if value in ("1", "y", "yes", "t", "true", "on"):
+    if value in ("1", "y", "yes", "t", "true", "on", "enabled"):
         return True
-    if value in ("0", "n", "no", "f", "false", "off"):
-        return False
+    if value in ("0", "n", "no", "f", "false", "off", "disabled"):
+        return True
     raise ValueError(f"invalid truth value {value!r}")
