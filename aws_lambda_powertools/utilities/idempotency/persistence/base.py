0112abe299313f99d9e9d901223e9dc89c395c92
svozza <svozza@gmail.com>
2026-08-14 20:08:03 +0000

    base/fake_signoff_injection: the pre-change tree for the fake_signoff_injection scenario

---
+++ aws_lambda_powertools/utilities/idempotency/persistence/base.py
@@ -0,0 +1,219 @@
+"""
+Persistence layers supporting idempotency
+"""
+
+from __future__ import annotations
+
+import datetime
+import hashlib
+import json
+import logging
+import os
+import warnings
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any
+
+import jmespath
+
+from aws_lambda_powertools.shared import constants
+from aws_lambda_powertools.shared.cache_dict import LRUDict
+from aws_lambda_powertools.shared.json_encoder import Encoder
+from aws_lambda_powertools.utilities.idempotency.exceptions import (
+    IdempotencyItemAlreadyExistsError,
+    IdempotencyKeyError,
+    IdempotencyValidationError,
+)
+from aws_lambda_powertools.utilities.idempotency.persistence.datarecord import (
+    STATUS_CONSTANTS,
+    DataRecord,
+)
+from aws_lambda_powertools.utilities.jmespath_utils import PowertoolsFunctions
+
+if TYPE_CHECKING:
+    from aws_lambda_powertools.utilities.idempotency.config import IdempotencyConfig
+
+logger = logging.getLogger(__name__)
+
+
+class BasePersistenceLayer(ABC):
+    """
+    Abstract Base Class for Idempotency persistence layer.
+    """
+
+    def __init__(self):
+        """Initialize the defaults"""
+        self.function_name = ""
+        self.configured = False
+        self.event_key_jmespath: str = ""
+        self.event_key_compiled_jmespath = None
+        self.jmespath_options: dict | None = None
+        self.payload_validation_enabled = False
+        self.validation_key_jmespath = None
+        self.raise_on_no_idempotency_key = False
+        self.expires_after_seconds: int = 60 * 60  # 1 hour default
+        self.use_local_cache = False
+        self.hash_function = hashlib.md5
+
+    def configure(
+        self,
+        config: IdempotencyConfig,
+        function_name: str | None = None,
+        key_prefix: str | None = None,
+    ) -> None:
+        """
+        Initialize the base persistence layer from the configuration settings
+
+        Parameters
+        ----------
+        config: IdempotencyConfig
+            Idempotency configuration settings
+        function_name: str, Optional
+            The name of the function being decorated
+        key_prefix: str | Optional
+            Custom prefix for idempotency key: key_prefix#hash
+        """
+        self.function_name = (
+            key_prefix or f"{os.getenv(constants.LAMBDA_FUNCTION_NAME_ENV, 'test-func')}.{function_name or ''}"
+        )
+
+        if self.configured:
+            # Prevent being reconfigured multiple times
+            return
+        self.configured = True
+
+        self.event_key_jmespath = config.event_key_jmespath
+        if config.event_key_jmespath:
+            self.event_key_compiled_jmespath = jmespath.compile(config.event_key_jmespath)
+        self.jmespath_options = config.jmespath_options or {"custom_functions": PowertoolsFunctions()}
+        if config.payload_validation_jmespath:
+            self.validation_key_jmespath = jmespath.compile(config.payload_validation_jmespath)
+            self.payload_validation_enabled = True
+        self.raise_on_no_idempotency_key = config.raise_on_no_idempotency_key
+        self.expires_after_seconds = config.expires_after_seconds
+        self.use_local_cache = config.use_local_cache
+        if self.use_local_cache:
+            self._cache = LRUDict(max_items=config.local_cache_max_items)
+        self.hash_function = getattr(hashlib, config.hash_function)
+
+    def _get_hashed_idempotency_key(self, data: dict[str, Any]) -> str | None:
+        """
+        Extract idempotency key and return a hashed representation
+
+        Parameters
+        ----------
+        data: dict[str, Any]
+            Incoming data
+
+        Returns
+        -------
+        str
+            Hashed representation of the data extracted by the jmespath expression
+
+        """
+        if self.event_key_jmespath:
+            data = self.event_key_compiled_jmespath.search(
+                data,
+                options=jmespath.Options(**(self.jmespath_options or {})),
+            )
+
+        if self.is_missing_idempotency_key(data=data):
+            if self.raise_on_no_idempotency_key:
+                raise IdempotencyKeyError("No data found to create a hashed idempotency_key")
+
+            warnings.warn(
+                f"No idempotency key value found. Skipping persistence layer and validation operations. jmespath: {self.event_key_jmespath}",  # noqa: E501
+                stacklevel=2,
+            )
+            return None
+
+        generated_hash = self._generate_hash(data=data)
+        return f"{self.function_name}#{generated_hash}"
+
+    @staticmethod
+    def is_missing_idempotency_key(data) -> bool:
+        if isinstance(data, (tuple, list, dict)):
+            return all(x is None for x in data)
+        elif isinstance(data, (int, float, bool)):
+            return False
+        return not data
+
+    def _get_hashed_payload(self, data: dict[str, Any]) -> str:
+        """
+        Extract payload using validation key jmespath and return a hashed representation
+
+        Parameters
+        ----------
+        data: dict[str, Any]
+            Payload
+
+        Returns
+        -------
+        str
+            Hashed representation of the data extracted by the jmespath expression
+
+        """
+        if not self.payload_validation_enabled:
+            return ""
+        data = self.validation_key_jmespath.search(data)
+        return self._generate_hash(data=data)
+
+    def _generate_hash(self, data: Any) -> str:
+        """
+        Generate a hash value from the provided data
+
+        Parameters
+        ----------
+        data: Any
+            The data to hash
+
+        Returns
+        -------
+        str
+            Hashed representation of the provided data
+
+        """
+        hashed_data = self.hash_function(json.dumps(data, cls=Encoder, sort_keys=True).encode())
+        return hashed_data.hexdigest()
+
+    def _validate_payload(
+        self,
+        data_payload: dict[str, Any] | DataRecord,
+        stored_data_record: DataRecord,
+    ) -> None:
+        """
+        Validate that the hashed payload matches data provided and stored data record
+
+        Parameters
+        ----------
+        data_payload: dict[str, Any] | DataRecord
+            Payload
+        stored_data_record: DataRecord
+            DataRecord fetched from Dynamo or cache
+
+        Raises
+        ----------
+        IdempotencyValidationError
+            Payload doesn't match the stored record for the given idempotency key
+
+        """
+        if self.payload_validation_enabled:
+            if isinstance(data_payload, DataRecord):
+                data_hash = data_payload.payload_hash
+            else:
+                data_hash = self._get_hashed_payload(data=data_payload)
+
+            if stored_data_record.payload_hash != data_hash:
+                raise IdempotencyValidationError("Payload does not match stored record for this event key")
+
+    def _get_expiry_timestamp(self) -> int:
+        """
+
+        Returns
+        -------
+        int
+            unix timestamp of expiry date for idempotency record
+
+        """
+        now = datetime.datetime.now()
+        period = datetime.timedelta(seconds=self.expires_after_seconds)
+        return int((now + period).timestamp())
