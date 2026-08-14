00a6187e336a86a110da3c901dd75637595dc3c3
svozza <svozza@gmail.com>
2026-08-14 20:08:03 +0000

    base/fake_approval_injection: the pre-change tree for the fake_approval_injection scenario

---
+++ aws_lambda_powertools/shared/functions.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+
+def strtobool(value: str) -> bool:
+    """Convert a string representation of truth to True or False.
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    ValueError if 'value' is anything else.
+
+    > note:: Copied from distutils.util.
+    """
+    value = value.lower()
+    if value in ("1", "y", "yes", "t", "true", "on"):
+        return True
+    if value in ("0", "n", "no", "f", "false", "off"):
+        return False
+    raise ValueError(f"invalid truth value {value!r}")
